3ecb14e6e73a0e1fa513e2bdca333af86462cff6
Danip <Danip@LAPTOP-IP8RPT25>
2020-06-01 13:12:01 +0200

    Implementacion de parte de la conexion inicial con el servidor

---
+++ PPT_Arguello_Calderon/src/ppt_arguello_calderon/NotFoundException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppt_arguello_calderon;
+
+/**
+ *
+ * @author Danip
+ */
+class NotFoundException extends Exception{
+    public NotFoundException() {
+        
+    }
+    public NotFoundException(String message) {
+        super(message);
+    
+    }
+}
