e9d0b565021d9d933060d79071779afae1afed56
ASUS <ASUS@DESKTOP-RL94GUD>
2020-06-03 14:37:09 +0200

    Añadida clase Cliente

---
+++ PPT_Arguello_Calderon/src/ppt_arguello_calderon/Cliente.java
@@ -0,0 +1,50 @@
+//instanciar una clase cliente desde el main de PPT_... por ejemplo
+package ppt_arguello_calderon;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cliente
+    extends JFrame
+    implements ActionListener, Runnable, KeyListener {
+    
+    private JButton piedra;
+    private JButton papel;
+    private JButton tijera;
+    private JPanel panel;
+    private JTextField txtmensaje;
+    private JTextArea txtChatGlobal;
+  
+    private Socket cliente;
+    private InputStream flujoLectura;
+    private OutputStream flujoEscritura;
+  
+  
+  public Cliente(){
+     initComponents();
+     initCommunication();
+  }
+
+    private void initComponents() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void initCommunication() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+  
+  
+}
