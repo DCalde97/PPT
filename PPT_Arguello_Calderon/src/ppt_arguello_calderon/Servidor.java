7b11a82add1131c48451ad2c5fd75f2d72731cbc
Danip <Danip@LAPTOP-IP8RPT25>
2020-07-16 18:40:10 +0200

    Cambios en las comunicaciones y en los inicios de las interfaces

--- PPT_Arguello_Calderon/src/ppt_arguello_calderon/Servidor.java
+++ PPT_Arguello_Calderon/src/ppt_arguello_calderon/Servidor.java
@@ -6,12 +6,17 @@
 package ppt_arguello_calderon;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,7 +29,7 @@ public class Servidor {
     
     public static void main (String args[]) throws Exception
     {
-        ServerSocket server = new ServerSocket(10001);
+        ServerSocket server = new ServerSocket(9998);
         System.out.println("Servidor Arrancado");
         
         /*
@@ -48,25 +53,41 @@ public class Servidor {
         }while (true);
         */
         
-        
         do{
             Socket sck =  server.accept();  
             System.out.println("Alguien conectado...");
-            Jugador J1 = Jugador.newJugador(sck);
+            Jugador J1 = Jugador.newJugador(sck,recNick(sck));
             jugadoresConectados.add(J1);
-            listaNicks(J1);//revisar en cliente
+            listaNicks(J1, sck);//revisar en cliente
 
       
         }while (true);
 
     }
+    
+    private static String recNick(Socket sck){
+        String nick=null;
+        try {
+            DataInputStream in = new DataInputStream( sck.getInputStream());
+            nick = in.readUTF();
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return nick;
+    }
 
-    private static void listaNicks(Jugador J1) {
+    private static void listaNicks(Jugador J1,Socket sck) {
         String listaNicks="";
         for(Jugador unJugador : jugadoresConectados){
             listaNicks = listaNicks.concat(unJugador.getNick()+"@");
         }
-        J1.sendMessage(listaNicks);
+        
+        try {
+            DataOutputStream out =new DataOutputStream( sck.getOutputStream());
+            out.writeUTF(listaNicks);
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public static Partida buscarPartida(int id) {
@@ -81,7 +102,7 @@ public class Servidor {
             }
             if (P==null){
                 NotFoundException ex;
-                throw ex=new NotFoundException ("Jugador no encontrado");
+                throw ex=new NotFoundException ("Partida no encontrada");
             }
         } catch(NotFoundException ex){
             P=null;
@@ -118,6 +139,14 @@ public class Servidor {
                 break;
             }
         }
+    }
+    
+    public int generarIdPartida(){
+        Partida P=null;
+            for(Partida unaPartida : partidasIniciadas){
+                    P=unaPartida;
+            }
+        return P.getIdent()+1;
     }
        
 }
